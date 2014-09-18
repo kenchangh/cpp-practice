5a8a1af7617d1f01aff3d6938138583819717c5f
Howard Chan <guanhao3797@gmail.com>
2014-09-18 19:12:56 +0800

    Optimized and tidied up

--- hot.cpp
+++ hot.cpp
@@ -1,6 +1,4 @@
 #include <Python.h>
-#include <iostream>
-#include <stdexcept>
 #include <ctime>
 #include <cmath>
 using namespace std;
@@ -14,27 +12,12 @@ unsigned long int epoch_seconds(tm datetime) {
   return epoch_seconds;
 }
 
-struct AnswerRemainder {
-  int answer;
-  int remainder;
-};
-
-AnswerRemainder divmod(long int a, long int b) {
-  AnswerRemainder result;
-  result.answer = a / b;
-  result.remainder = a % b;
-  return result;
-}
-
 float score_rate(int score, long int seconds) {
-  AnswerRemainder minutes_seconds, hours_minutes;
   long int minutes, hours;
 
-  minutes_seconds = divmod(seconds, 60);
-  minutes = minutes_seconds.answer;
-  hours_minutes = divmod(minutes, 60);
-  hours = hours_minutes.answer;
-  minutes = hours_minutes.remainder;
+  minutes = seconds / 60;
+  hours = minutes / 60;
+  minutes = minutes % 60;
 
   float rate;
   hours != 0
@@ -47,7 +30,7 @@ double round(double value, int nth) {
   return floor(value*10*nth + 0.5)/(10*nth);
 }
 
-static PyObject* hot(PyObject *self, PyObject *args) {
+static PyObject* hot_score(PyObject *self, PyObject *args) {
   int score, comments;
   float created;
 
@@ -76,36 +59,18 @@ static PyObject* hot(PyObject *self, PyObject *args) {
 }
 
 static PyMethodDef module_methods[] = {
-  {"hot", (PyCFunction)hot, METH_VARARGS, NULL}
+  {"hot", (PyCFunction)hot_score, METH_VARARGS,
+   "Calculates a post's hot score."},
+  {NULL, NULL, 0, NULL}
 };
 
-PyMODINIT_FUNC inithot(void) {
-  (void) Py_InitModule("hot", module_methods);
+PyMODINIT_FUNC inithot_score(void) {
+  (void) Py_InitModule("hot_score", module_methods);
 }
 
-int main(int argc, char *argv[]) {
+/*int main(int argc, char *argv[]) {
   Py_SetProgramName(argv[0]);
   Py_Initialize();
   inithot();
-  return 0;
-}
-
-/*int main() {
-  time_t time_now;
-  // Creating some random time
-  tm some_time;
-  some_time.tm_sec = 50;
-  some_time.tm_min = 50;
-  some_time.tm_hour = 20;
-  some_time.tm_mday = 20;
-  some_time.tm_mon = 10;
-  some_time.tm_year = 150;
-  some_time.tm_wday = 1;
-  some_time.tm_yday = 50;
-  some_time.tm_isdst = 5;
-
-  // Calculating post's hot_score
-  float hot_score = hot(60, some_time, 5);
-  cout << hot_score << endl;
   return 0;
 }*/
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup, Extension
-setup(name='hot', version='1.0',
-      ext_modules=[Extension('hot', ['hot.cpp'])])
+setup(name='hot_score', version='1.0',
+      ext_modules=[Extension('hot_score', ['hot.cpp'])])
